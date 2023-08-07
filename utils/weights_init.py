758161e396eb8d2047095c97634723f685a3f413
ddb324 <jiangdaoran@163.com>
2023-08-07 11:47:32 +0800

    docs: Add comments to improve function clarity
    Added comprehensive comments to the init_model() function
    for better understanding of its logic and purpose.
    
    - Documented function parameters and return values
    - Explained the steps of price calculation
    
    No functional changes were made; this commit only
    improves code documentation.

--- utils/weights_init.py
+++ utils/weights_init.py
@@ -64,7 +64,12 @@ def init_weights(net, init_type='normal'):
         raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
 
 
+# 这个方法的目的是帮助模型在训练开始时获得合适的初始参数值，这样有助于更快地收敛和更好的训练效果
 def init_model(net):
+    # 检查net是否是nn.Conv3d或者nn.ConvTranspose3d的实例
     if isinstance(net, nn.Conv3d) or isinstance(net, nn.ConvTranspose3d):
+        # 使用Kaiming正态分布初始化方法初始化权重。
+        # Kaiming初始化通常适用于使用ReLU激活函数的层，这里的0.25是一个用于缩放的参数，它控制了初始化分布的标准差
         nn.init.kaiming_normal_(net.weight.data, 0.25)
+        # 用常数值0来初始化偏置
         nn.init.constant_(net.bias.data, 0)
